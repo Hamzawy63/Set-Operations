2d27c2068a5130f017bfd10209c498d7d075ac8a
Hamzawy63 <hamzahassan835@gmail.com>
2019-10-06 21:52:26 +0200

    add interface methods

--- src/DataItem.java
+++ src/DataItem.java
@@ -1,17 +1,22 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class DataItem {
-    HashMap<String , Integer> universe; // hamzawyyyyyyyyyyyy
-    int n ; // length of the universe
-    boolean arr[][]; // length of this array is 26 * n
+    HashMap<String , Integer> universe;
+    int universeSize ; // length of the universe
+    boolean subsets[][]; // length of this array is 26 * n
+    Scanner sc ;
 
     public DataItem(HashMap<String, Integer> universe) {
         this.universe = universe;
-        this.n = universe.size() ;
+        this.universeSize = universe.size() ;
+        subsets =  new boolean[26][universeSize];
+        sc = new Scanner(System.in);
     }
     public void AddSubset(char name )
     {
         name = Character.toUpperCase(name) ;
+
     }
 }
--- src/SetOperations.java
+++ src/SetOperations.java
@@ -1,3 +1,8 @@
 public interface SetOperations {
+    public void union(DataItem data, char first, char second);
+
+    public void intersection(DataItem data, char first, char second);
+
+    public void difference(DataItem data, char first, char second);
 
 }
