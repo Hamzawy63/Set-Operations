a525413285cc2f4608c8fc0537448afcebd174e3
Hamzawy63 <hamzahassan835@gmail.com>
2019-10-06 21:11:06 +0200

    add data class

--- src/DataItem.java
+++ src/DataItem.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DataItem {
     HashMap<String , Integer> universe;
+    int n ; // length of the universe
+    int arr[][]; // length of this array is 26 * n
 
+    public DataItem(HashMap<String, Integer> universe) {
+        this.universe = universe;
+        this.n = universe.size() ;
+    }
+    public void AddSubset(char name )
+    {
+
+    }
 }
