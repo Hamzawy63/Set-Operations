e2b6034a96cc7e01e1a3b70b3b6bc1c7adf850f6
Hamzawy63 <hamzahassan835@gmail.com>
2019-10-06 22:05:40 +0200

    add operations and some test code for dataset entry

--- src/DataItem.java
+++ src/DataItem.java
@@ -8,15 +8,37 @@ public class DataItem {
     boolean subsets[][]; // length of this array is 26 * n
     Scanner sc ;
 
-    public DataItem(HashMap<String, Integer> universe) {
-        this.universe = universe;
+    public DataItem() {
+        this.universe = new HashMap<String , Integer>();
         this.universeSize = universe.size() ;
         subsets =  new boolean[26][universeSize];
         sc = new Scanner(System.in);
     }
+    public void addUniverse()
+    {
+        System.out.println("Enter The Length of your universe");
+        int n = sc.nextInt();
+        System.out.println("Enter your universe elements one at a line ");
+        for (int i = 0; i <n ; i++) {
+            universe.put(sc.nextLine() , i);
+        }
+    }
     public void AddSubset(char name )
     {
+        System.out.println("Enter the name of your subset ");
         name = Character.toUpperCase(name) ;
+        System.out.println("Enter the size  of your subset ");
+        int size = sc.nextInt();
+        System.out.println("Enter your subset elements ");
+        for (int i = 0; i <size ; i++) {
+            String s = sc.nextLine();
+            while (!universe.containsKey(s))
+            {
+                System.out.println("invalid entry");
+                s = sc.nextLine();
+            }
+        }
 
     }
+
 }
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    /*
+    Test code
+     */
+    public static void main(String[] args) {
+        DataItem data = new DataItem();
+        data.addUniverse();
+    }
+}
