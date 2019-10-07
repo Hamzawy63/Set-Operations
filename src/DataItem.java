bad8e209b3cf548d59c9ca4b53772209d0f6c52e
Hamzawy63 <hamzahassan835@gmail.com>
2019-10-07 20:49:03 +0200

    some edits in dataItem class

--- src/DataItem.java
+++ src/DataItem.java
@@ -3,44 +3,87 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class DataItem {
-    HashMap<String , Integer> universe; // to have index when you have string
+    HashMap<String, Integer> universe; // to have index when you have string
     ArrayList<String> universeId; // to have string when you have index
-    int universeSize ; // length of the universe
+    int universeSize; // length of the universe
     boolean subsets[][]; // length of this array is 26 * n
-    Scanner sc ;
+    private Scanner sc;
+    private boolean[] subsetNames;
+    private int numberOfSubsets;
+
+
 
     public DataItem() {
-        this.universe = new HashMap<String , Integer>();
-        this.universeSize = universe.size() ;
-        subsets =  new boolean[26][universeSize];
+        this.universe = new HashMap<String, Integer>();
+        subsets = new boolean[26][universeSize];
         sc = new Scanner(System.in);
-        universeId=new ArrayList<>();
+        universeId = new ArrayList<>();
+        subsetNames = new boolean[26];
+        numberOfSubsets = 0;
     }
-    public void addUniverse()
-    {
+    public void addUniverse() {
         System.out.println("Enter The Length of your universe");
         int n = sc.nextInt();
         System.out.println("Enter your universe elements one at a line ");
-        for (int i = 0; i <n ; i++) {
-            universe.put(sc.nextLine() , i);
-        }
-    }
-    public void AddSubset(char name )
-    {
-        System.out.println("Enter the name of your subset ");
-        name = Character.toUpperCase(name) ;
-        System.out.println("Enter the size  of your subset ");
-        int size = sc.nextInt();
-        System.out.println("Enter your subset elements ");
-        for (int i = 0; i <size ; i++) {
-            String s = sc.nextLine();
-            while (!universe.containsKey(s))
-            {
-                System.out.println("invalid entry");
-                s = sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String input = sc.nextLine();
+            if (universe.containsKey(input)) {
+                i--;
+                n--;
+            } else {
+                universe.put(input, i);
+                universeId.add(input);
             }
         }
+        this.universeSize = n;
+
 
     }
+
+    public void addSubset()
+    {
+        if(numberOfSubsets == 26) // then we have used all the characters
+        {
+            System.out.println("Sorry you have used all the names");
+            return;
+        }
+
+        System.out.println("Enter Name of your subset (just one character)");
+        char name = Character.toLowerCase(sc.nextLine().charAt(0));
+        while( !(name>='A' && name <= 'Z') ||  subsetNames[name - 'A'])
+        {
+
+            System.out.println("Invalid input or this character is taken ");
+            System.out.println("Enter Name of your subset (just one character)");
+             name = Character.toLowerCase(sc.nextLine().charAt(0));
+        }
+        numberOfSubsets++;
+        System.out.println("Enter size of your subset ");
+        int n =  sc.nextInt();
+        System.out.println("Enter elements of your subsets line by  line");
+        for (int i = 0; i <n ; i++) {
+            String input = sc.nextLine();
+            while(universe.get(input) == null)
+            {
+                System.out.println("This element is not in the universe enter a valid input ");
+                input = sc.nextLine();
+            }
+
+            int idx  = universe.get(input);
+
+            // add the element in our
+            subsets[name - 'A'][idx] = true;
+
+
+
+
+
+
+
+        }
+
+
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     Test code
      */
     public static void main(String[] args) {
-        DataItem data = new DataItem();
-        data.addUniverse();
+//        DataItem data = new DataItem();
+//        data.addUniverse();
     }
 }
